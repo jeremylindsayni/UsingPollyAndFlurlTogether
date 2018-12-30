03a21f2a81bd3621af6132b391c536baf3bee9f2
Jeremy Lindsay <jeremy.lindsay@outlook.com>
2018-12-30 23:13:24 +0000

    Improved retry policy.

--- MyWebsite/Policies.cs
+++ MyWebsite/Policies.cs
@@ -1,10 +1,11 @@
-﻿using System.Diagnostics;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Polly;
+﻿using Polly;
 using Polly.Retry;
 using Polly.Timeout;
 using Polly.Wrap;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace MyWebsite
 {
@@ -16,7 +17,7 @@ namespace MyWebsite
             {
                 return Policy.TimeoutAsync<HttpResponseMessage>(2, (context, timeSpan, task) =>
                 {
-                    Debug.WriteLine("[App|Policy]: Timeout delegate fired after " + timeSpan.TotalMilliseconds);
+                    Debug.WriteLine($"[App|Policy]: Timeout delegate fired after {timeSpan.Seconds} seconds");
                     return Task.CompletedTask;
                 });
             }
@@ -26,13 +27,20 @@ namespace MyWebsite
         {
             get
             {
-                return Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                return Policy
+                    .HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
                     .Or<TimeoutRejectedException>()
-                    .RetryAsync(3, (delegateResult, i) =>
-                    {
-                        Debug.WriteLine("[App|Policy]: Retry delegate fired, attempt " + i);
-                        return Task.CompletedTask;
-                    });
+                    .WaitAndRetryAsync(new[]
+                        {
+                            TimeSpan.FromSeconds(1),
+                            TimeSpan.FromSeconds(2),
+                            TimeSpan.FromSeconds(5)
+                        },
+                        (delegateResult, retryCount) =>
+                        {
+                            Debug.WriteLine(
+                                $"[App|Policy]: Retry delegate fired, attempt {retryCount}");
+                        });
             }
         }
 
